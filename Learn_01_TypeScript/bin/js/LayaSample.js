9e980835fe7943c0752dd5f7cf3a92abd0964b89
zengfeng <zengfeng@shinezone.com>
2018-05-31 09:36:10 +0800

    no message

---
+++ Learn_01_TypeScript/bin/js/LayaSample.js
@@ -0,0 +1,11 @@
+var WebGL = Laya.WebGL;
+// 程序入口
+var GameMain = /** @class */ (function () {
+    function GameMain() {
+        Laya.init(600, 400, WebGL);
+        console.log("Hello World");
+    }
+    return GameMain;
+}());
+new GameMain();
+//# sourceMappingURL=LayaSample.js.map
---
+++ Learn_01_TypeScript/src/LayaSample.ts
@@ -0,0 +1,10 @@
+import WebGL = Laya.WebGL;
+// 程序入口
+class GameMain{
+    constructor()
+    {
+        Laya.init(600,400, WebGL);
+        console.log("Hello World");
+    }
+}
+new GameMain();
