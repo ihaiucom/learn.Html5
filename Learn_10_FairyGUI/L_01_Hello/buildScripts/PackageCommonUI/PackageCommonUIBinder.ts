6665b5b816fdeaec66d715cfbcfafae6be687ad3
ihaiu <ihaiu@qq.com>
2018-06-12 22:31:01 +0800

    no message

---
+++ Learn_10_FairyGUI/L_01_Hello/buildScripts/PackageCommonUI/PackageCommonUIBinder.ts
@@ -0,0 +1,10 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+module TestFGUI.PackageCommonUI {
+
+	export class PackageCommonUIBinder{
+		public static bindAll():void {
+
+		}
+	}
+}
