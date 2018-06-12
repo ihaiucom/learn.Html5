fa82d684c1d0f406b08470f0cd4bf1d9ed796603
曾峰 <zengfeng@shinezone.com>
2018-06-12 22:33:04 +0800

    no message

---
+++ Learn_10_FairyGUI/L_01_Hello/buildScripts/Package_GUIS_Common/Package_GUIS_CommonBinder.ts
@@ -0,0 +1,10 @@
+/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
+
+module TestFGUI.Package_GUIS_Common {
+
+	export class Package_GUIS_CommonBinder{
+		public static bindAll():void {
+
+		}
+	}
+}
